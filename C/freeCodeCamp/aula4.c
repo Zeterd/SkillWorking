9f3a3e93d0c8b16ff8600def4e0e9bd0e69bb582
Zeterd <jpsousa13@gmail.com>
2020-09-24 16:49:36 +0100

    math

---
+++ C/freeCodeCamp/aula4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char const *argv[]) {
+
+	printf("The square root of %d is %f\n", 25, sqrt(25));
+	printf("Result: %f\n", pow(3, 2));
+	printf("%f\n", ceil(2.56));
+	printf("%f\n", floor(2.56));
+
+	
+
+
+
+	return 0;
+}
