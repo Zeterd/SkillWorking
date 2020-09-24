85bb2ad6fdba9b134f32d56fccd51183615345b9
Zeterd <jpsousa13@gmail.com>
2020-09-24 16:56:55 +0100

    input

---
+++ C/freeCodeCamp/aula6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+	
+	int age;
+
+	printf("Please enter your age: ");
+	scanf("%d", &age);
+	printf("Yor age is %d\n", age);
+
+
+	return 0;
+}
