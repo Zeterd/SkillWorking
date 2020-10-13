21e399b1a7ae530bd83333293b9d3db2d7d3e4d4
Zeterd <jpsousa13@gmail.com>
2020-10-13 17:06:52 +0100

    script cpu complete

--- Bash/Projects/tempCPU.sh
+++ Bash/Projects/tempCPU.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 sensors > temp.txt
-var=$(tail -n 19 temp.txt | head -n 1 | awk '{print $4}')
 
-temp=$(echo "$var" | fold -w1 | head -n 3 | tail -n 2 | paste -sd "")
+var=$(cat /proc/cpuinfo | grep "model name" | head -n1 | awk '{print $4}')
+cpu_brand=${var::-3}
+
+cpu_model=$(cat /proc/cpuinfo | grep "model name" | head -n1 | awk '{print $6}')
+
+cpu_num_cores=$(expr $(cat /proc/cpuinfo | grep "processor" | wc -l) - 4)
+
+cpu_num_threads=$(cat /proc/cpuinfo | grep "processor" | wc -l)
+
+cpu_speed=$(cat /proc/cpuinfo | grep "model name" | head -n1 | awk '{print $9}')
+
+cpu_temp=$(tail -n 19 temp.txt | head -n 1 | awk '{print $4}' | fold -w1 | head -n 3 | tail -n 2 | paste -sd "")
+
 cpu_fan=$(tail -n 3 temp.txt | head -n 1 | awk '{print $2}')
 
+echo ""
 echo "########### Information of CPU ###########"
 echo ""
-echo -e "Brand of CPU:\t\tNVIDIA"
-echo -e "Model:\t\t\t$gpu_name"
-echo -e "Temperature of GPU is:\t$gpu_temp"
-echo -e "Memory (usage/total):\t$mem_usg / $mem_total"
-echo -e "GPU usage:\t\t$gpu_usage"
+echo -e "Brand of GPU:\t\t\t$cpu_brand"
+echo -e "Model of CPU:\t\t\t$cpu_model"
+echo -e "Number of cores/threads:\t$cpu_num_cores/$cpu_num_threads"
+echo -e "Speed of CPU(GHz):\t\t$cpu_speed"
+echo -e "Temperature of CPU is:\t\t$cpu_tempºC"
+echo ""
